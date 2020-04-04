5f07045afdb291a27c56059ee4c80ed4eadd5a3a
tirastudio <62678863+tirastudio@users.noreply.github.com>
2020-04-05 01:30:08 +0700

    Update podcasts.js

--- data/podcasts.js
+++ data/podcasts.js
@@ -1,11 +1,11 @@
-import egghead from '../content/images/thefeed.png'
+import egghead from '../content/images/egghead.png'
 import hashnode from '../content/thumbnails/hashnode.svg'
 
 export default [
   {
     title: 'Lejitkan Nilai Brand Anda dengan Levidio The Feed',
     path: 'https://a.rootpixel.net/YBP2plJD',
-    img: thefeed,
+    img: egghead,
   },
   {
     title: 'Woman in Tech: Tania Rascia - with Bolaji Ayodeji of Hashnode',
