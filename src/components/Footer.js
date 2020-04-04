b834d8a60d8ebca9b0cbbf98798112245206fa46
tirastudio <62678863+tirastudio@users.noreply.github.com>
2020-04-04 19:18:47 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,19 +9,19 @@ export default class Footer extends Component {
     return (
       <footer className="footer container">
         <div>
-          <a href="https://ko-fi.com/taniarascia" target="_blank" rel="noopener noreferrer">
+          <a href="#" target="_blank" rel="noopener noreferrer">
             Ko-Fi
           </a>
-          <a href="https://patreon.com/taniarascia" target="_blank" rel="noopener noreferrer">
+          <a href="#" target="_blank" rel="noopener noreferrer">
             Patreon
           </a>
           <Link to="/contact">Newsletter</Link>
-          <a href="https://www.taniarascia.com/rss.xml" target="_blank" rel="noopener noreferrer">
+          <a href="https://agussetiyobudi.my.id/rss.xml" target="_blank" rel="noopener noreferrer">
             RSS
           </a>
         </div>
         <div>
-          <a href="https://github.com/taniarascia" title="Open-source on GitHub">
+          <a href="https://github.com/" title="Open-source on GitHub">
             <img
               src={github}
               target="_blank"
