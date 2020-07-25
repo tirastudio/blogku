b32ce1aa8198d6f6a678b9417eb42636845b6463
tirastudio <62678863+tirastudio@users.noreply.github.com>
2020-07-25 11:09:20 +0700

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -2,57 +2,57 @@ export default [
   {
     icon: '🏬',
     title: 'Toko Online',
-    path: 'https://api.whatsapp.com/send?phone=+6288279325589&text=Halo, saya mau buat toko online, bisa di infokan proses pemesanan nya ?',
+    path: 'https://api.whatsapp.com/send?phone=+6288271477642&text=Halo, saya mau buat toko online, bisa di infokan proses pemesanan nya ?',
     description: 'Paket Pembuatan Toko Online, Fitur Lengkap, Gratis Domain',
   },
   {
     icon: '🚀',
     title: 'Tour Travel',
-    path: 'https://api.whatsapp.com/send?phone=+6288279325589&text=Hola, saya mau pesan website tour travel, bisa di infokan harga nya ?',
+    path: 'https://api.whatsapp.com/send?phone=+6288271477642&text=Hola, saya mau pesan website tour travel, bisa di infokan harga nya ?',
     description: 'Untuk meningkatkan penjualan paket wisata anda dibutuhkan website dengan desain unik & menarik 😍',
   },
   {
     icon: '🚗',
     title: 'Dealer & Rent Car',
-    path: 'https://api.whatsapp.com/send?phone=+6288279325589&text=Hola, saya mau buat website dealer atau sales mobil, kira2 berapa biaya keseluruhannya ?',
+    path: 'https://api.whatsapp.com/send?phone=+6288271477642&text=Hola, saya mau buat website dealer atau sales mobil, kira2 berapa biaya keseluruhannya ?',
     description: 'Website Sales Otomotif, sales Mobil dan Motor dengan harga Murah dan Berkualitas',
   },
   {
     icon: '🏢',
     title: 'Company Profil',
-    path: 'https://api.whatsapp.com/send?phone=+6288279325589&text=Halo, saya mau bikin website company profil, detailnya pemesanan nya gimana ya ?',
+    path: 'https://api.whatsapp.com/send?phone=+6288271477642&text=Halo, saya mau bikin website company profil, detailnya pemesanan nya gimana ya ?',
     description: 'Mau bikin website company profile buat perusahaan?',
   },
   {
     icon: '🎓',
     title: 'Sekolah',
-    path: 'https://api.whatsapp.com/send?phone=+6288279325589&text=Hola, saya mau konsultasi tentang pembuatan website sekolah, detail pemesanannya bagaimana ya ?',
+    path: 'https://api.whatsapp.com/send?phone=+6288271477642&text=Hola, saya mau konsultasi tentang pembuatan website sekolah, detail pemesanannya bagaimana ya ?',
     description: 'Jadikan dunia pendidikan lebih informatif dengan website sekolah',
   },
   {
     icon: '🏠',
     title: ' Property',
-    path: 'https://api.whatsapp.com/send?phone=+6288279325589&text=Hola, untuk harga pemesanan website property mulai berapa ya ?',
+    path: 'https://api.whatsapp.com/send?phone=+6288271477642&text=Hola, untuk harga pemesanan website property mulai berapa ya ?',
     description:
       'Bikin website property single agen atau multi agen dengan fitur lengkap dan desain modern.',
   },
   {
     icon: '📝',
     title: 'Blog Pribadi',
-    path: 'https://api.whatsapp.com/send?phone=+6288279325589&text=Halo, mau bikin blog pribadi berapaan ya ?',
+    path: 'https://api.whatsapp.com/send?phone=+6288271477642&text=Halo, mau bikin blog pribadi berapaan ya ?',
     description: 'Hobi nulis, males ribet di teknis, udah bikin disini aja 😎',
   },
   {
     icon: '🌀',
     title: 'Membership',
-    path: 'https://api.whatsapp.com/send?phone=+6288279325589&text=Halo, saya mau pesan website membership / affilisi, apakah bisa ?',
+    path: 'https://api.whatsapp.com/send?phone=+6288271477642&text=Halo, saya mau pesan website membership / affilisi, apakah bisa ?',
     description:
       'Butuh website membership + program afiliasi? konsultasi aja dulu 👌',
   },
   {
     icon: '💯',
     title: 'LandingPage',
-    path: 'https://api.whatsapp.com/send?phone=+6288279325589&text=Hola, bikin landingpage siap pake mulai berapaan ya ?',
+    path: 'https://api.whatsapp.com/send?phone=+6288271477642&text=Hola, bikin landingpage siap pake mulai berapaan ya ?',
     description: 'Lagi cari jasa pembuatan landingpage yang benar-benar berkonversi tinggi? udah chat aja langsung 💬',
   },
 ]
